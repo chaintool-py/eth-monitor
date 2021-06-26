41d38d4eb8b4ab89f56695d5c1732caadf0c7745
nolash <dev@holbrook.no>
2021-06-26 14:04:34 +0200

    Initial commit

---
+++ eth_monitor/chain.py
@@ -0,0 +1,18 @@
+# external imports
+from chainlib.interface import ChainInterface
+from chainlib.eth.block import (
+        block_by_number,
+        Block,
+        )
+from chainlib.eth.tx import (
+        receipt,
+        Tx,
+        )
+
+class EthChainInterface(ChainInterface):
+
+    def __init__(self):
+        self._block_by_number = block_by_number
+        self._block_from_src = Block.from_src
+        self._tx_receipt = receipt
+        self._src_normalize = Tx.src_normalize
